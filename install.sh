441e0265d67ee7512f231249e3cdea8fcfde284c
Babak Parvizi <bparvizi@amazon.com>
2024-10-21 14:28:46 -0700

    Added script to install base architecture stacks.

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+EMAIL="user@saascostworkshop.com"
+
+# Deploy the SaaS control plane.
+cd saas-control-plane/scripts
+./deploy.sh "$EMAIL"
+cd ../../
+
+# Deploy the application plane shared services.
+cd saas-app-plane/shared-services/scripts/
+./deploy.sh
+cd ../../../
+
+# Deploy the serverless product and order microservices.
+cd saas-app-plane/product-service/scripts/
+./deploy.sh
+cd ../../../
+
+# Deploy the product review service.
+cd saas-app-plane/product-review-service/scripts/
+./deploy.sh
+cd ../../../
+
+# Deploy the product media stack.
+cd saas-app-plane/product-media-service/scripts/
+./deploy.sh
+cd ../../../
