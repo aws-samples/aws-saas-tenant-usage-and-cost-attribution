d2264664304cbeaf1e0b178b2fd0785f317b1bb7
Anubhav Sharma <shaanubh@amazon.com>
2024-11-08 10:15:12 -0500

    fix for region

--- saas-control-plane/scripts/deploy.sh
+++ saas-control-plane/scripts/deploy.sh
@@ -7,8 +7,12 @@ if [[ -z "$CDK_PARAM_SYSTEM_ADMIN_EMAIL" ]]; then
   exit 1
 fi
 
-
-REGION=$(aws configure get region)
+export AWS_REGION=$(aws configure get region)
+if [ -z "$AWS_REGION" ]; then
+  export TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds:60")
+  export AWS_REGION=$(curl -H "X-aws-ec2-metadata-token:${TOKEN}" -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/') && echo "export AWS_REGION=${AWS_REGION}" >> /home/ec2-user/.bashrc
+fi
+echo "REGION: ${AWS_REGION}"
 
 # Preprovision base infrastructure
 cd ../cdk
